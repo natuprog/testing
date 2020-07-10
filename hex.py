97578b3134ae1f9eb60972e35f41d6091556c6c4
natuprog <relifenatu@gmail.com>
2020-07-10 17:04:55 +0200

    version 1

---
+++ hex.py
@@ -0,0 +1,15 @@
+import math as m
+
+def hex (hexa):
+        
+
+print("Write the color in hex: ")
+mensaje=input()
+hexa=''
+if (mensaje[0] == '#'):
+    if (len(mensaje) == 7):
+        i=0;
+        while (i < 6):
+            hexa[0] = mensaje[1+i]
+            hexa[1] = mensaje[2+i]
+             
